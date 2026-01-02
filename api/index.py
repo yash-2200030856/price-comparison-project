c66f06c8b00acef1f6dbb5d7ffa89f719172d3af
yash-2200030856 <yashulagisetty@gmail.com>
2026-01-02 12:48:15 +0530

    Fix Django import in API

--- api/index.py
+++ api/index.py
@@ -1,4 +1,16 @@
-from BestPrice.Supermarket.wsgi import application
+import sys
+import os
 
-# Vercel expects 'app' or 'application'
+# Add the BestPrice directory to the path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'BestPrice'))
+
+# Set Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Supermarket.settings')
+
+# Import Django WSGI application
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
+
+# Vercel expects 'app'
 app = application
